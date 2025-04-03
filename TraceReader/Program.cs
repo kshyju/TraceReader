617e9ec28b06a541653033595e5dbbf2194556c2
Shyju Krishnankutty <connectshyju@gmail.com>
2025-04-03 09:56:08 -0700

    Add project files.

---
+++ TraceReader/Program.cs
@@ -0,0 +1,16 @@
+﻿namespace TraceReader
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello, World!");
+            var filePath =args.Length > 0 ? args[0] : @"C:\devtools\Perfview\funchost_windows.nettrace";
+
+            //filePath = @"C:\devtools\Perfview\PerfViewData_4.1039.100-ci.25175.0_HelloHttpNet9NoProxy.etl";
+
+            var reader = new TraceReader(filePath);
+            reader.ReadTrace();
+        }
+    }
+}
---
+++ TraceReader/TraceReader.cs
@@ -0,0 +1,52 @@
+﻿
+using Microsoft.Diagnostics.Tracing;
+using Microsoft.Diagnostics.Tracing.Etlx;
+
+namespace TraceReader
+{
+    internal sealed class TraceReader(string filePath)
+    {
+        private string _etlxPath = string.Empty;
+
+        public void ReadTrace()
+        {
+            if (filePath.EndsWith(".nettrace"))
+            {
+                _etlxPath = TraceLog.CreateFromEventPipeDataFile(filePath);
+                Console.WriteLine($"Converted to: {_etlxPath}");
+            }
+            else
+            {
+                _etlxPath = filePath;
+            }
+
+            using (TraceLog traceLog = TraceLog.OpenOrConvert(_etlxPath, new TraceLogOptions { KeepAllEvents = true }))
+            {
+                var diagnosticSourceEvents = traceLog.Events
+                    .Where(e => e.ProviderName == "Microsoft-Diagnostics-DiagnosticSource")
+                    .ToList();
+
+                var activityEvents = diagnosticSourceEvents
+                    .Where(e => e.EventName.Contains("Activity"))
+                    .ToList();
+
+                Console.WriteLine($"Activity Events Count: {activityEvents.Count}");
+                foreach (var traceEvent in activityEvents)
+                {
+                    Console.WriteLine($"{traceEvent.EventName}, Time: {traceEvent.TimeStamp}, ProcessName:{GetProcessName(traceLog, traceEvent)}");
+                }
+            }
+        }
+
+        private string GetProcessName(TraceLog traceLog, TraceEvent traceEvent)
+        {
+            var process = traceLog.Processes.FirstOrDefault(p => p.ProcessID == traceEvent.ProcessID);
+            if (process != null)
+            {
+                return process.Name;
+            }
+
+            return traceEvent.ProcessName;
+        }
+    }
+}
